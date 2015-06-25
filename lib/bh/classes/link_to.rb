5046ae0e1487b8b73337574fdbc492302b2a90b7
Robert Westwood <rjw@catalystcomputing.co.uk>
2015-06-25 15:40:35 +0100

    Internal fix for bh not adding active classes to the nav when run with middleman build

--- lib/bh/classes/link_to.rb
+++ lib/bh/classes/link_to.rb
@@ -12,9 +12,25 @@ module Bh
         case Bh.framework
         when :rails
           @app.current_page? @url
-        when :padrino, :middleman
-          request = Bh.framework == :middleman ? @app.req : @app.request
+        when :padrino
+          request =  @app.request
           request.path_info == @app.url_for(@url)
+        when :middleman
+          return false if @url.nil?
+          url_s = @url.to_s # We expect url to be a string, but just in case
+          # The middleman-generated path may or may not end with a slash (depending)
+          #  on the setting of set :trailing_slash; in either case @url can be a
+          #  user-generated url which may or may not end in the way that matches
+          #  that of middleman-generated paths
+          if url_s.end_with?('/')
+            url_with_slash = url_s
+            url_without_slash = url_s.chomp('/')
+          else
+            url_without_slash = url_s
+            url_with_slash = url_s + '/'
+          end
+          current_resource_url = @app.url_for(@app.current_resource)
+          current_resource_url == @app.url_for(url_with_slash) || current_resource_url == @app.url_for(url_without_slash)
         end
       end
 
